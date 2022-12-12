affc2e1f27c06eda9247e31305dab47f5ce6bbda
TakaHasui <emeth.2501@gmail.com>
2022-12-12 11:21:16 +0900

    Initial commit

---
+++ component/header.js
@@ -0,0 +1,36 @@
+document.write(`
+<!--
+  https://getbootstrap.jp/docs/5.0/components/navbar/
+  https://qumeru.com/magazine/562
+-->
+<header>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <h1><a class="navbar-brand" href="#">チクラグループ</a></h1>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="#">チクラグループ</a>
+          <a class="nav-link" href="#">株式会社チクラ工業</a>
+          <a class="nav-link" href="#">熊本鋼機株式会社</a>
+          <a class="nav-link" href="#">玉名金属株式会社</a>
+          <!--
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"
+            >Disabled</a
+          >-->
+        </div>
+      </div>
+    </div>
+  </nav>
+</header>
+`);
---
+++ readme.md
@@ -0,0 +1,27 @@
+# 対象
+
+- http://www.chikura.jp/
+
+# 概要
+
+- https://drive.google.com/drive/folders/1e7gOjsTQgKhmrJs4f1b476mnf6yoLZ32
+- https://docs.google.com/document/d/1eKHB8zRjBSLj5wqYL5zzB_TmFQsbUi2HyIWBYRcNjOk/edit
+
+# 12/3 打ち合わせ
+
+- ヘッダーメニューはページ内リンク
+- 各会社リンクはフッターに
+- 三社一心
+  - テキストだけ載せる。
+  - 各会社はフッターに社名と事業だけ載せる
+  - 会社への地図は、自前のものがあればそれを使って、Google マップへのリンクを別でつける。
+  - ファビコンは？
+    - ロゴがあるか確認してもらう
+    - なし
+
+# その他
+
+- Bootstrap のよく使うクラスまとめ
+  - https://qiita.com/nakanishi03/items/c80a16b9b9796c25f890
+- DL を Table っぽく
+  - https://code-log.net/css/dl-dt-dd/
